a85d07a7ef9bdc0204d687e393cd72f00b759a20
Mike <u3568882@connect.hku.hk>
2022-06-28 10:33:48 +0800

    goooo

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
-from setuptools import setup
-if __name__ == "__main__":
-    setup()
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+	requirements = f.read().splitlines()
+
+setup(
+    name="bigtwo",
+    version="0.1.22",
+    packages=find_packages(include=["bigtwo", "bigtwo.*"]),
+    description="Python tutorial",
+    author="Mike",
+    author_email="mikecheng2001@gmail.com",
+    url="http://youtube.com/",
+    install_requires=requirements
+)
